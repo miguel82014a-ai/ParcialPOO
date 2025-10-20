ce2bf741df1246c0879cfc1aa226e65088e497e0
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-20 07:43:33 -0500

    En la clase Estudiante añadi su codigo

--- src/main/java/Modelo/Estudiante.java
+++ src/main/java/Modelo/Estudiante.java
@@ -8,6 +8,23 @@ package Modelo;
  *
  * @author migue
  */
-public class Estudiante {
+public class Estudiante extends Persona{
+    private String grado;
+    private double promedio;
+    
+    public Estudiante(String nombre, String direccion, String telefono, String fechaNacimiento, String grado, double promedio) {
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.grado = grado;
+        this.promedio = promedio;
+    }
+    
+
+    public String obtenerDatosFormateados() {
+        return " DATOS DEL ESTUDIANTE \n" +
+               super.obtenerDatosFormateados() + "\n" +
+               "Grado: " + grado + "\n" +
+               "Promedio: " + promedio + "\n" +
+               "------------------------";
+    }
     
 }
