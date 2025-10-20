faa3981ec5bba7dad4a08a78500ae6a40ef1eb0a
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-20 07:24:55 -0500

    Cree el codigo, con un paquete Modelo donde estaran las subclases

---
+++ src/main/java/com/mycompany/parcialpoo/ParcialPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcialpoo;
+
+/**
+ *
+ * @author migue
+ */
+public class ParcialPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
