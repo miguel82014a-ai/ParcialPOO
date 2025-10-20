d37f44fd4b337a9bdc32b172af72b230cf4dabed
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-20 07:44:10 -0500

    En la clase Profesor añadi su codigo

--- src/main/java/Modelo/Profesor.java
+++ src/main/java/Modelo/Profesor.java
@@ -8,6 +8,40 @@ package Modelo;
  *
  * @author migue
  */
-public class Profesor {
+public class Profesor extends Persona {
+        private String cedula;
+    private String area;
+    private double salarioHora;
+    private int horasTrabajadas;
+    
+    public Profesor(String nombre, String direccion, String telefono, String fechaNacimiento, String cedula, String area, double salarioHora, int horasTrabajadas) {
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.cedula = cedula;
+        this.area = area;
+        this.salarioHora = salarioHora;
+        this.horasTrabajadas = horasTrabajadas;
+    }
+    
+
+    public double calcularSalario() {
+        double salarioBase = salarioHora * horasTrabajadas;
+        double preparacionClase = salarioBase * 0.20;
+        return salarioBase + preparacionClase;
+    }
+    
+
+    public double calcularPrestaciones() {
+        double salarioTotal = calcularSalario();
+        return salarioTotal * 0.17;
+    }
+    
+    public String obtenerDatosFormateados() {
+        return "=== DATOS DEL PROFESOR ===\n" +
+               super.obtenerDatosFormateados() + "\n" +
+               "Cédula: " + cedula + "\n" +
+               "Área: " + area + "\n" +
+               "Salario por Hora: $" + String.format("%,.0f", salarioHora) + "\n" +
+               "Horas Trabajadas: " + horasTrabajadas;
+    }
     
 }
