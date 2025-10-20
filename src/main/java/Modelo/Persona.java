37e87593ebf6a4cbf3f9857a4e35476ee802756f
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-20 07:43:53 -0500

    En la clase Persona añadi su codigo

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -9,5 +9,22 @@ package Modelo;
  * @author migue
  */
 public class Persona {
+    protected String nombre;
+    protected String direccion;
+    protected String telefono;
+    protected String fechaNacimiento;
     
+    public Persona(String nombre, String direccion, String telefono, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public String obtenerDatosFormateados() {
+        return "Nombre: " + nombre + "\n" +
+               "Dirección: " + direccion + "\n" +
+               "Teléfono: " + telefono + "\n" +
+               "Fecha Nacimiento: " + fechaNacimiento;
+    }
 }
